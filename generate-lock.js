234b9e5b58ed0e21a82a456787c712020b5507df
Shubham Goel <ass@gmail.com>
2025-07-26 19:41:27 +0530

    fix build to pages

---
+++ generate-lock.js
@@ -0,0 +1,3 @@
+// This script helps generate package-lock.json for deployment
+console.log('Run: npm install --package-lock-only');
+console.log('Then commit the generated package-lock.json file');
